68e4c09ec4c6ea64e4c49c056766193a8fe3625b
jagodakurosad <jagoda.kurosad@gmail.com>
2025-06-17 20:55:43 +0200

    working bell tray icon

--- src/main.rs
+++ src/main.rs
@@ -9,35 +9,21 @@ enum Message {
 
 fn main() {
     let mut tray = TrayItem::new(
-        "Tray Example",
-        IconSource::Resource("name-of-icon-in-rc-file"),
+        "JNotify",
+        IconSource::Resource("bell"),
     )
         .unwrap();
 
-    tray.add_label("Tray Label").unwrap();
+    tray.add_label("Jenkins Notification App").unwrap();
 
-    tray.add_menu_item("Hello", || {
-        println!("Hello!");
+    tray.add_menu_item("Show", || {
+        println!("show!");
     })
         .unwrap();
 
     tray.inner_mut().add_separator().unwrap();
 
     let (tx, rx) = mpsc::sync_channel(1);
-
-    let red_tx = tx.clone();
-    tray.add_menu_item("Red", move || {
-        red_tx.send(Message::Red).unwrap();
-    })
-        .unwrap();
-
-    let green_tx = tx.clone();
-    tray.add_menu_item("Green", move || {
-        green_tx.send(Message::Green).unwrap();
-    })
-        .unwrap();
-
-    tray.inner_mut().add_separator().unwrap();
 
     let quit_tx = tx.clone();
     tray.add_menu_item("Quit", move || {
@@ -50,16 +36,6 @@ fn main() {
             Ok(Message::Quit) => {
                 println!("Quit");
                 break;
-            }
-            Ok(Message::Red) => {
-                println!("Red");
-                tray.set_icon(IconSource::Resource("another-name-from-rc-file"))
-                    .unwrap();
-            }
-            Ok(Message::Green) => {
-                println!("Green");
-                tray.set_icon(IconSource::Resource("name-of-icon-in-rc-file"))
-                    .unwrap()
             }
             _ => {}
         }
