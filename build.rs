2e643b56c7c1b282c293c7c99cc46db78d1a784e
Blajszu <oskar.blajsz@gmail.com>
2025-06-22 22:15:55 +0200

    refactor 2

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 extern crate embed_resource;
 
 fn main() {
-    let _ = embed_resource::compile("tray-example.rc", embed_resource::NONE);
+    let _ = embed_resource::compile("resources.rc", embed_resource::NONE);
     slint_build::compile("ui/app-window.slint").expect("Slint build failed");
 }
